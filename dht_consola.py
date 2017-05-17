d154cd602eba2bf091bc7c9ed1a9a604249a9412
Jose Zorrilla <jzorrilla@x-red.com>
2017-05-17 18:24:25 +1000

    Agrego comentarios

--- dht_consola.py
+++ dht_consola.py
@@ -19,15 +19,20 @@ sensor = Adafruit_DHT.DHT11
 # Configuracion del puerto GPIO al cual esta conectado
 pin = 23
 
+# Intenta ejecutar las siguientes instrucciones, si falla va a la instruccion except
 try:	
-	# Ciclo principal
+	# Ciclo principal infinito
 	while True:
-		
+		# Obtiene la humedad y la temperatura desde el sensor 
 		humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
-
+		
+		# Imprime en la consola las variables temperatura y humedad con un decimal
 		print('Temperatura={0:0.1f}*  Humedad={1:0.1f}%'.format(temperatura, humedad))
 
+		# Duerme 10 segundos
 		time.sleep(10)
 
+# Se ejecuta en caso de que falle alguna instruccion dentro del try
 except Exception,e:
+	# Imprime en pantalla el error e
 	print str(e)
