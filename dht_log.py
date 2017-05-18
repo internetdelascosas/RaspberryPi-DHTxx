fc5dcf0c5fc4f1e9b17c7393f3d304d171c8e6ee
Jose Zorrilla <jzorrilla@x-red.com>
2017-05-19 02:06:49 +1000

    Agrego script dht_log.py

--- dht_log.py
+++ dht_log.py
@@ -7,32 +7,46 @@
 # Autor        : Jose Zorrilla <jzorrilla@iot.cl>
 # Dependencias : Libreria de Adafruit https://github.com/adafruit/Adafruit_Python_DHT
 # Web          : http://internetdelascosas.cl/
- 
+
 # Importa las librerias necesarias 
-import sys
 import time
+import datetime
 import Adafruit_DHT
+
+# Log file
+log_path = "/var/log/iot/"
 
 # Configuracion del tipo de sensor DHT
 sensor = Adafruit_DHT.DHT11
 
-# Configuracion del puerto GPIO al cual esta conectado  (GPIO 23)
+# Configuracion del puerto GPIO al cual esta conectado (GPIO 23)
 pin = 23
 
+# Escribe un archivo log en log_path con el nombre en el formato yyyy-mm-dd_dht.log
+def write_log(text):
+	log = open(log_path + datetime.datetime.now().strftime("%Y-%m-%d") + "_dht.log","a")
+	line = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + text + "\n"
+	log.write(line)
+	log.close()
+
 # Intenta ejecutar las siguientes instrucciones, si falla va a la instruccion except
-try:	
+try:
 	# Ciclo principal infinito
 	while True:
 		# Obtiene la humedad y la temperatura desde el sensor 
 		humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
-		
-		# Imprime en la consola las variables temperatura y humedad con un decimal
-		print('Temperatura={0:0.1f}*  Humedad={1:0.1f}%'.format(temperatura, humedad))
+
+		# Si obtiene una lectura del sensor la registra en el archivo log
+		if humedad is not None and temperatura is not None:
+			write_log("DHT Sensor - Temperatura: %s" % str(temperatura))
+			write_log("DHT Sensor - Humedad:  %s" % str(humedad))
+		else:
+			write_log('Error al obtener la lectura del sensor')
 
 		# Duerme 10 segundos
 		time.sleep(10)
 
 # Se ejecuta en caso de que falle alguna instruccion dentro del try
 except Exception,e:
-	# Imprime en pantalla el error e
-	print str(e)
+	# Registra el error en el archivo log y termina la ejecucion
+	write_log(str(e))
