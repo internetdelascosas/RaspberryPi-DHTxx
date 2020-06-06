db28952c27b798ae57451a26b43a6d05c9235a4c
Jose Zorrilla <jzorrilla@x-red.com>
2020-06-06 23:35:16 +1000

    Version python3

--- dht_mysql.py
+++ dht_mysql.py
@@ -1,25 +1,30 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # Internet de las Cosas - http://internetdelascosas.cl
 #
 # Descripcion  : Programa que permite obtener la lectura de un sensor DHT11 y registra los valores
 #				 en una base de datos MySQL
-# Lenguaje     : Python
+# Lenguaje     : Python version 3
 # Autor        : Jose Zorrilla <jzorrilla@iot.cl>
-# Dependencias : Libreria de Adafruit https://github.com/adafruit/Adafruit_Python_DHT
-# Web          : http://internetdelascosas.cl/
+# Dependencias : Libreria Circuit Python de Adafruit https://github.com/adafruit/Adafruit_CircuitPython_DHT
+# Web          : https://www.internetdelascosas.cl/2017/05/19/raspberry-pi-conectando-un-sensor-de-temperatura-y-humedad-dht11/
 
-# Importa las librerias necesarias 
+# Importa las librerias necesarias
 import time
 import datetime
-import Adafruit_DHT
+import adafruit_dht
 import MySQLdb
 
 # Importa la configuracion
 from config import *
 
-# Configuracion del tipo de sensor DHT
-sensor = Adafruit_DHT.DHT11
+# Configuracion del puerto GPIO al cual esta conectado (GPIO 23)
+pin = 23
+
+# Crea el objeto para acceder al sensor
+# se debe descomentar la linea dependiendo del tipo de sensor (DHT11 o DHT22)
+#sensor = adafruit_dht.DHT11(pin)
+sensor = adafruit_dht.DHT22(pin)
 
 # Inserta in registro en la base de datos MySQL
 def write_mysql(temperatura, humedad):
@@ -52,25 +57,33 @@ def write_log(text):
 	log.write(line)
 	log.close()
 
-# Intenta ejecutar las siguientes instrucciones, si falla va a la instruccion except
-try:
+# Funcion principal
+def main():
 	# Ciclo principal infinito
 	while True:
-		# Obtiene la humedad y la temperatura desde el sensor 
-		humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
 
-		# Si obtiene una lectura del sensor la registra en el archivo log
-		if humedad is not None and temperatura is not None:
-			write_log("DHT Sensor - Temperatura: %s" % str(temperatura))
-			write_log("DHT Sensor - Humedad:  %s" % str(humedad))
-			write_mysql(temperatura,humedad)
-		else:
-			write_log('Error al obtener la lectura del sensor')
+		# Intenta ejecutar las siguientes instrucciones, si falla va a la instruccion except
+		try:
+			# Obtiene la humedad y la temperatura desde el sensor
+			humedad = sensor.humidity
+			temperatura = sensor.temperature
+
+			# Si obtiene una lectura del sensor la registra en el archivo log
+			if humedad is not None and temperatura is not None:
+				write_log("DHT Sensor - Temperatura: %s" % str(temperatura))
+				write_log("DHT Sensor - Humedad:  %s" % str(humedad))
+				write_mysql(temperatura,humedad)
+			else:
+				write_log('Error al obtener la lectura del sensor')
+
+		# Se ejecuta en caso de que falle alguna instruccion dentro del try
+		except RuntimeError as error:
+			# Imprime en pantalla el error
+			print(error.args[0])
 
 		# Duerme 10 segundos
 		time.sleep(10)
 
-# Se ejecuta en caso de que falle alguna instruccion dentro del try
-except Exception,e:
-	# Registra el error en el archivo log y termina la ejecucion
-	write_log(str(e))
+# Llama a la funcion principal
+if __name__ == "__main__":
+    main()
